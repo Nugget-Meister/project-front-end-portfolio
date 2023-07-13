744ea0954a06b0fcdeeee0e5d27f40bf8e9b4bfa
Nugget-Meister <tremainecamacho@gmail.com>
2023-07-12 20:49:03 -0400

    page and js setup

---
+++ index.js
@@ -0,0 +1,11 @@
+let base_url = "https://www.swapi.tech/api/"
+let ships_url = "https://www.swapi.tech/api/starships/"
+let vehicles_url = "https://www.swapi.tech/api/vehicles"
+
+let examplegoogle = "https://www.google.com/search?tbm=isch&q=Sand+Crawler"
+
+console.log(base_url)
+
+function getVehicles() {}
+
+function getShips() {}
